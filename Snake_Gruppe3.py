46fe91402d5bdfa3cb5d4e99c23c0e1437d9ea9c
Marcello <tambasciam@gmail.com>
2024-10-29 08:40:25 +0100

    Update Snake_Gruppe3.py

--- Snake_Gruppe3.py
+++ Snake_Gruppe3.py
@@ -9,16 +9,17 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Einfaches Snake-Spiel')
 
-
 snake_size = 20
 snake_pos = [100, 50]
 snake_body = [[100, 50], [90, 50], [80, 50]]
 direction = 'RIGHT'
-change_to = direction
 
 apple_size = 20
 apple_pos = [random.randint(0, (screen_width // apple_size) - 1) * apple_size,
-random.randint(0, (screen_height // apple_size) - 1) * apple_size]
+             random.randint(0, (screen_height // apple_size) - 1) * apple_size]
+
+# Variable für das Wachstum
+wachsen = 0
 
 while True:
     for event in pygame.event.get():
@@ -47,10 +48,21 @@ while True:
     elif direction == 'UP':
         snake_pos[1] -= snake_size
 
+    # Snake-Körper aktualisieren
     snake_body.insert(0, list(snake_pos))
-    snake_body.pop()
+
+    # Überprüfen, ob die Snake einen Apfel gesammelt hat
+    if snake_pos == apple_pos:
+        wachsen += 1
+
+        apple_pos = [random.randint(0, (screen_width // apple_size) - 1) * apple_size,
+                     random.randint(0, (screen_height // apple_size) - 1) * apple_size]
+    else:
+        # Letztes Segment entfernen, wenn kein Wachstum
+        snake_body.pop()
 
     # Snake zeichnen
+    screen.fill((0, 0, 0))
     for pos in snake_body:
         pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(pos[0], pos[1], snake_size, snake_size))
 
